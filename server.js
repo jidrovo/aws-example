3238d1296fef232218ad2947ecf9168a0909eec4
Jhon <benjaminjosejose10@gmail.com>
2022-02-08 12:05:35 -0500

    texto modificado

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
 
-app.get('/',(req,res)=> res.send('Hello World with express!'))
+app.get('/',(req,res)=> res.send('<h1>Express con HTML!</h1>'))
 app.listen(3000);
 console.log("Server on port 3000");
